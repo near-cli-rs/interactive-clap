b831cbca45cd9fd3342b87b948e188ba22d4a477
dj8yf0μl <noreply@nowhere.org>
2025-01-07 23:53:36 +0200

    doc: add doc-string about dbg_cond! macro

--- interactive-clap-derive/src/debug.rs
+++ interactive-clap-derive/src/debug.rs
@@ -5,6 +5,16 @@ macro_rules! dbg_cond {
     };
 }
 
+/// this macro under `introspect` feature can be used to debug how derive proc macros
+/// ([`crate::InteractiveClap`], [`crate::ToCliArgs`])
+/// work similar to the following:
+///
+/// ```bash
+/// # interactive-clap-derive folder
+/// cargo test test_doc_comments_propagate --features introspect -- --nocapture
+/// # from repo root
+/// cargo run --example struct_with_subargs --features interactive-clap-derive/introspect
+/// ```
 #[cfg(not(feature = "introspect"))]
 macro_rules! dbg_cond {
     ($($val:expr),*) => {
