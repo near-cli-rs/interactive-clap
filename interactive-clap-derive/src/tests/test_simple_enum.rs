5dc36c10a40da2c2787529b0b3a850182ca22980
dj8yf0μl <noreply@nowhere.org>
2025-02-05 00:26:57 +0200

    test: forwardport test+snapshots for test_simple_enum from `master`

--- interactive-clap-derive/src/tests/test_simple_enum.rs
+++ interactive-clap-derive/src/tests/test_simple_enum.rs
@@ -1,7 +1,6 @@
 use super::pretty_codegen;
 
 #[test]
-#[ignore]
 fn test_simple_enum() {
     let input = syn::parse_quote! {
         pub enum Mode {
@@ -15,7 +14,16 @@ fn test_simple_enum() {
     let interactive_clap_codegen = crate::derives::interactive_clap::impl_interactive_clap(&input);
     insta::assert_snapshot!(pretty_codegen(&interactive_clap_codegen));
 
-    let to_cli_args_codegen = crate::derives::to_cli_args::impl_to_cli_args(&input);
+    let step_one_output = syn::parse_quote! {
+        pub enum CliMode {
+            /// Prepare and, optionally, submit a new transaction with online mode
+            Network,
+            /// Prepare and, optionally, submit a new transaction with offline mode
+            Offline,
+        }
+    };
+
+    let to_cli_args_codegen = crate::derives::to_cli_args::impl_to_cli_args(&step_one_output);
     insta::assert_snapshot!(pretty_codegen(&to_cli_args_codegen));
 }
 
