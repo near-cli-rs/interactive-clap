ac291292e471d749bbffff728de5bf4f759b794f
FroVolod <frol_off@meta.ua>
2025-12-10 10:17:35 +0200

    updated examples for "cliclack"

---
+++ examples/advanced_struct_cliclack.rs
@@ -0,0 +1,170 @@
+// This example shows additional functionality of the "interactive-clap" macro for parsing command-line data into a structure using macro attributes.
+
+// 1) build an example: cargo build --example advanced_struct_cliclack
+// 2) go to the `examples` folder: cd target/debug/examples
+// 3) run an example: ./advanced_struct_cliclack (without parameters) => entered interactive mode
+//                    ./advanced_struct_cliclack --age-full-years 30 --first-name QWE --second-name QWERTY --favorite-color red =>
+//                                    => cli_args: CliArgs { age: Some(30), first_name: Some("QWE"), second_name: Some("QWERTY"), favorite_color: Some(Red) }
+//                    ./advanced_struct_cliclack --first-name QWE --second-name QWERTY --favorite-color red =>
+//                                    => cli_args: CliArgs { age: None, first_name: Some("QWE"), second_name: Some("QWERTY"), favorite_color: Some(Red) }
+// To learn more about the parameters, use "help" flag: ./advanced_struct_cliclack --help
+
+use cliclack::{input, select};
+use interactive_clap::{ResultFromCli, ToCliArgs};
+use strum::{EnumDiscriminants, EnumIter, EnumMessage, IntoEnumIterator};
+
+#[derive(Debug, Clone, interactive_clap::InteractiveClap)]
+struct Args {
+    #[interactive_clap(long = "age-full-years")]
+    #[interactive_clap(skip_interactive_input)]
+    /// If you want, enter the full age on the command line
+    age: Option<u64>,
+    #[interactive_clap(long)]
+    /// What is your first name?
+    first_name: String,
+    #[interactive_clap(long)]
+    #[interactive_clap(skip_default_input_arg)]
+    second_name: String,
+    #[interactive_clap(long)]
+    #[interactive_clap(value_enum)]
+    #[interactive_clap(skip_default_input_arg)]
+    favorite_color: ColorPalette,
+}
+
+impl Args {
+    fn input_second_name(_context: &()) -> color_eyre::eyre::Result<Option<String>> {
+        match input("Input second name").interact() {
+            Ok(value) => Ok(Some(value)),
+            Err(err) if err.kind() == std::io::ErrorKind::Interrupted => Ok(None),
+            Err(err) => Err(err.into()),
+        }
+    }
+
+    fn input_favorite_color(_context: &()) -> color_eyre::eyre::Result<Option<ColorPalette>> {
+        let variants = ColorPaletteDiscriminants::iter()
+            .map(|v| (v, v, ""))
+            .collect::<Vec<_>>();
+        let selected = select("What color is your favorite?")
+            .items(&variants)
+            .interact()?;
+        match selected {
+            ColorPaletteDiscriminants::Red => Ok(Some(ColorPalette::Red)),
+            ColorPaletteDiscriminants::Orange => Ok(Some(ColorPalette::Orange)),
+            ColorPaletteDiscriminants::Yellow => Ok(Some(ColorPalette::Yellow)),
+            ColorPaletteDiscriminants::Green => Ok(Some(ColorPalette::Green)),
+            ColorPaletteDiscriminants::Blue => Ok(Some(ColorPalette::Blue)),
+            ColorPaletteDiscriminants::Indigo => Ok(Some(ColorPalette::Indigo)),
+            ColorPaletteDiscriminants::Violet => Ok(Some(ColorPalette::Violet)),
+        }
+    }
+}
+
+#[derive(Debug, EnumDiscriminants, Clone, clap::ValueEnum)]
+#[strum_discriminants(derive(EnumMessage, EnumIter))]
+pub enum ColorPalette {
+    #[strum_discriminants(strum(message = "red"))]
+    /// Red
+    Red,
+    #[strum_discriminants(strum(message = "orange"))]
+    /// Orange
+    Orange,
+    #[strum_discriminants(strum(message = "yellow"))]
+    /// Yellow
+    Yellow,
+    #[strum_discriminants(strum(message = "green"))]
+    /// Green
+    Green,
+    #[strum_discriminants(strum(message = "blue"))]
+    /// Blue
+    Blue,
+    #[strum_discriminants(strum(message = "indigo"))]
+    /// Indigo
+    Indigo,
+    #[strum_discriminants(strum(message = "violet"))]
+    /// Violet
+    Violet,
+}
+
+impl interactive_clap::ToCli for ColorPalette {
+    type CliVariant = ColorPalette;
+}
+
+impl std::str::FromStr for ColorPalette {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "red" => Ok(Self::Red),
+            "orange" => Ok(Self::Orange),
+            "yellow" => Ok(Self::Yellow),
+            "green" => Ok(Self::Green),
+            "blue" => Ok(Self::Blue),
+            "indigo" => Ok(Self::Indigo),
+            "violet" => Ok(Self::Violet),
+            _ => Err("ColorPalette: incorrect value entered".to_string()),
+        }
+    }
+}
+
+impl std::fmt::Display for ColorPalette {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::Red => write!(f, "red"),
+            Self::Orange => write!(f, "orange"),
+            Self::Yellow => write!(f, "yellow"),
+            Self::Green => write!(f, "green"),
+            Self::Blue => write!(f, "blue"),
+            Self::Indigo => write!(f, "indigo"),
+            Self::Violet => write!(f, "violet"),
+        }
+    }
+}
+
+impl std::fmt::Display for ColorPaletteDiscriminants {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::Red => write!(f, "red"),
+            Self::Orange => write!(f, "orange"),
+            Self::Yellow => write!(f, "yellow"),
+            Self::Green => write!(f, "green"),
+            Self::Blue => write!(f, "blue"),
+            Self::Indigo => write!(f, "indigo"),
+            Self::Violet => write!(f, "violet"),
+        }
+    }
+}
+
+fn main() -> color_eyre::Result<()> {
+    ctrlc::set_handler(move || {}).expect("setting Ctrl-C handler");
+
+    let mut cli_args = Args::parse();
+    let context = (); // default: input_context = ()
+    loop {
+        let args = <Args as interactive_clap::FromCli>::from_cli(Some(cli_args), context);
+        match args {
+            ResultFromCli::Ok(cli_args) | ResultFromCli::Cancel(Some(cli_args)) => {
+                println!("cli_args: {cli_args:?}");
+                println!(
+                    "Your console command:  {}",
+                    shell_words::join(cli_args.to_cli_args())
+                );
+                return Ok(());
+            }
+            ResultFromCli::Cancel(None) => {
+                println!("Goodbye!");
+                return Ok(());
+            }
+            ResultFromCli::Back => {
+                cli_args = Default::default();
+            }
+            ResultFromCli::Err(cli_args, err) => {
+                if let Some(cli_args) = cli_args {
+                    println!(
+                        "Your console command:  {}",
+                        shell_words::join(cli_args.to_cli_args())
+                    );
+                }
+                return Err(err);
+            }
+        }
+    }
+}
---
+++ examples/struct_with_context_cliclack.rs
@@ -0,0 +1,126 @@
+// This example shows additional functionality of the "interactive-clap" macro for parsing command line data into a structure using the context attributes of the macro.
+
+// 1) build an example: cargo build --example struct_with_context_cliclack
+// 2) go to the `examples` folder: cd target/debug/examples
+// 3) run an example: ./struct_with_context_cliclack (without parameters) => entered interactive mode
+//                    ./struct_with_context_cliclack account QWERTY => offline_args: Ok(OfflineArgs { account: Sender { sender_account_id: "QWERTY" } })
+// To learn more about the parameters, use "help" flag: ./struct_with_context_cliclack --help
+
+use interactive_clap::{ResultFromCli, ToCliArgs};
+
+mod simple_enum;
+
+#[derive(Debug, Clone)]
+pub enum ConnectionConfig {
+    Testnet,
+    Mainnet,
+    Betanet,
+}
+
+#[derive(Debug, Clone, interactive_clap::InteractiveClap)]
+#[interactive_clap(input_context = ())]
+#[interactive_clap(output_context = OfflineArgsContext)]
+pub struct OfflineArgs {
+    #[interactive_clap(named_arg)]
+    ///Specify a sender
+    sender: Sender,
+}
+
+#[derive(Debug)]
+pub struct OfflineArgsContext {
+    pub some_context_field: i64,
+}
+
+impl OfflineArgsContext {
+    fn from_previous_context(
+        _previous_context: (),
+        _scope: &<OfflineArgs as interactive_clap::ToInteractiveClapContextScope>::InteractiveClapContextScope,
+    ) -> color_eyre::eyre::Result<Self> {
+        Ok(Self {
+            some_context_field: 42,
+        })
+    }
+}
+
+impl From<OfflineArgsContext> for NetworkContext {
+    fn from(_: OfflineArgsContext) -> Self {
+        Self {
+            connection_config: None,
+        }
+    }
+}
+
+impl From<()> for NetworkContext {
+    fn from(_: ()) -> Self {
+        Self {
+            connection_config: None,
+        }
+    }
+}
+
+impl From<NetworkContext> for () {
+    fn from(_: NetworkContext) -> Self {}
+}
+
+#[derive(Debug)]
+pub struct NetworkContext {
+    pub connection_config: Option<ConnectionConfig>,
+}
+
+#[derive(Debug, Clone, interactive_clap::InteractiveClap)]
+#[interactive_clap(context = NetworkContext)]
+pub struct Sender {
+    #[interactive_clap(skip_default_input_arg)]
+    sender_account_id: String,
+    #[interactive_clap(subcommand)]
+    network: simple_enum::Mode,
+}
+
+impl Sender {
+    fn input_sender_account_id(
+        context: &NetworkContext,
+    ) -> color_eyre::eyre::Result<Option<String>> {
+        println!("Let's use context: {:?}", context);
+        match cliclack::input("What is the account ID?").interact() {
+            Ok(value) => Ok(Some(value)),
+            Err(err) if err.kind() == std::io::ErrorKind::Interrupted => Ok(None),
+            Err(err) => Err(err.into()),
+        }
+    }
+}
+
+fn main() -> color_eyre::Result<()> {
+    let mut cli_offline_args = OfflineArgs::parse();
+    let context = (); // #[interactive_clap(input_context = ())]
+    loop {
+        let offline_args = <OfflineArgs as interactive_clap::FromCli>::from_cli(
+            Some(cli_offline_args.clone()),
+            context,
+        );
+        match offline_args {
+            ResultFromCli::Ok(cli_offline_args) | ResultFromCli::Cancel(Some(cli_offline_args)) => {
+                println!(
+                    "Your console command:  {}",
+                    shell_words::join(cli_offline_args.to_cli_args())
+                );
+                return Ok(());
+            }
+            ResultFromCli::Cancel(None) => {
+                println!("Goodbye!");
+                return Ok(());
+            }
+            ResultFromCli::Back => {
+                cli_offline_args = Default::default();
+            }
+            ResultFromCli::Err(cli_offline_args, err) => {
+                if let Some(cli_offline_args) = cli_offline_args {
+                    println!(
+                        "Your console command:  {}",
+                        shell_words::join(cli_offline_args.to_cli_args())
+                    );
+                }
+                return Err(err);
+            }
+        }
+    }
+}
